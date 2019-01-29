cc7a6a19a6fe8c00f46f939bf00ac732de96862d
Carman Zhang <carmanzhang@yunify.com>
2019-01-29 19:52:21 +0800

    refactor client sql

--- pkg/option/option.go
+++ pkg/option/option.go
@@ -3,16 +3,19 @@ package option
 import "flag"
 
 var (
-	MysqlHost             = flag.String("mysql", "127.0.0.1", "")
-	MysqlPort             = flag.String("mysql_port", "3306", "")
-	Database              = flag.String("database", "alert_client", "")
-	User                  = flag.String("user", "root", "")
-	Password              = flag.String("password", "password", "")
-	DispatcherServiceHost = flag.String("dispatcher_service", "alerting-dispatcher-server.kubesphere-monitoring-system.svc", "")
-	DispatcherServicePort = flag.String("dispatcher_port", "50000", "")
+	MysqlHost = flag.String("mysql", "127.0.0.1", "")
+	MysqlPort = flag.String("mysql_port", "3306", "")
+	Database  = flag.String("database", "alert_client", "")
+	User      = flag.String("user", "root", "")
+	Password  = flag.String("password", "password", "")
+	//DispatcherServiceHost = flag.String("dispatcher_service", "alerting-dispatcher-server.kubesphere-monitoring-system.svc", "")
+	//DispatcherServicePort = flag.String("dispatcher_port", "50000", "")
 
-	//DispatcherServiceHost = flag.String("dispatcher_service", "139.198.120.226", "")
-	//DispatcherServicePort = flag.String("dispatcher_port", "35000", "")
+	DispatcherServiceHost = flag.String("dispatcher_service", "139.198.120.226", "")
+	DispatcherServicePort = flag.String("dispatcher_port", "35000", "")
+
+	//DispatcherServiceHost = flag.String("dispatcher_service", "172.31.140.133", "")
+	//DispatcherServicePort = flag.String("dispatcher_port", "50000", "")
 )
 
 func init() {
